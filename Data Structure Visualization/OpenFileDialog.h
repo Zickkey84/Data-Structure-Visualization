affe18cd60828ce6fcd643d2ee3fbba9b7b037f2
Trần Phước Hải <haitran080475@gmail.com>
2024-08-29 05:39:19 +0700

    Small Update, and add Sample

--- Data Structure Visualization/OpenFileDialog.h
+++ Data Structure Visualization/OpenFileDialog.h
@@ -5,6 +5,7 @@
 #include <commdlg.h>
 #include <string.h>
 #include <filesystem>
+
 inline std::string ConvertToNarrowString(const std::wstring& wstr) {
     int size_needed = WideCharToMultiByte(CP_UTF8, 0, &wstr[0], (int)wstr.size(), NULL, 0, NULL, NULL);
     std::string strTo(size_needed, 0);
